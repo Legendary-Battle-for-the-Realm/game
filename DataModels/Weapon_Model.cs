f42aa43b3c04653d25099aa726f339b230c58687
TKira4 <toan01224551919@gmail.com>
2025-04-13 16:38:54 +0700

    thêm submodule vào

--- DataModels/Weapon_Model.cs
+++
@@ -1,9 +0,0 @@
-public class Weapon {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Atk { get; set; }   // Dạng phần trăm
-    public string Desc { get; set; }
-    public int OwnerId { get; set; }
-    public string CultivationRequired { get; set; }
-    public EquipmentSkill Skill { get; set; }
-}
